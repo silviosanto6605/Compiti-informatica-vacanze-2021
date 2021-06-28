48eb95c77fe058044908729f1af075d79034ed70
Silvio Santoriello <silviosanto6605@gmail.com>
2021-06-28 18:18:28 +0200

    es 26

---
+++ 26.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{   
+    int arrsize, contaSottopeso, contaSovrappeso, contaPesoCorretto;
+    float peso, pesoTotaleNorma, pesoTotaleFuoriNorma;
+    const float PESORNORMA = 1.0;
+
+
+    cout<<"Quanti pacchetti si hanno? ";
+    cin>>arrsize;
+
+
+    float pesi[arrsize]; 
+    
+
+
+    for(int i=0; i<arrsize;i++){
+
+        cout<<"Inserisci un peso: ";
+        cin>>peso;
+        if(peso == 0){i=arrsize+1;}
+        else{
+            pesi[arrsize] = peso;
+        }
+
+    }   
+
+
+    for(int j=0; j<arrsize; j++){
+        if(pesi[j]>PESORNORMA+(PESORNORMA*100)/2){ /*fuori norma - sovrappeso*/
+            contaSovrappeso += 1;
+            pesoTotaleFuoriNorma += pesi[j];
+
+
+        } 
+        
+        else if(pesi[j]<PESORNORMA+(PESORNORMA*100)/2){ /*fuori norma - sottopeso*/
+            contaSottopeso += 1;
+            pesoTotaleFuoriNorma += pesi[j];
+
+
+        }
+        
+        else { /*a norma */
+            contaPesoCorretto += 1;
+            pesoTotaleNorma += pesi[j];
+
+
+        }
+
+
+
+
+    }
+
+
+    cout<<"Pacchetti con peso corretto: " << contaPesoCorretto<<endl;
+    cout<<"Peso totale della merce con peso a norma: " << pesoTotaleNorma<<endl<<endl;
+
+
+    cout<<"Pacchetti sottopeso: " << contaSottopeso<<endl;
+    cout<<"Pacchetti sovrappeso: " << contaSovrappeso<<endl;
+    cout<<"Peso totale della merce con peso non a norma: " << pesoTotaleFuoriNorma<<endl<<endl;
+    
+}
+
